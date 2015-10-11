f8df5efe4a1cf56fe251e83d81d4057cb96388b1
Zain Munad <zainm211@hotmail.com>
2015-10-11 03:35:27 +0000

    names work now

--- server.js
+++ server.js
@@ -3,10 +3,16 @@ var app = require("express")();
 var http = require("http").Server(app);
 //socket.io will take care of sending clientside socket code to the client
 var io = require("socket.io")(http);
+var unirest = require('unirest');
 
 //handle http requests to the website
 app.get('/', function(req, res){
   res.sendFile('/home/ubuntu/workspace/' + 'frontend/index.html');
+});
+
+//this is a NASTY fix, i hate it
+app.get('/main.css', function(req, res){
+  res.sendFile('/home/ubuntu/workspace/' + 'frontend/main.css');
 });
 
 //handles socket client connection
@@ -14,8 +20,20 @@ io.on('connection', function(socket){
   console.log('a user connected');
   
   socket.on('chat message', function(msg) {
-      console.log("user says: " + msg);
+      console.log(msg.name + " says: " + msg.message);
+      //send message to be displayed on all connected clients (bad lol)
       io.emit('chat message', msg);
+      
+      //send message to chatbot for processing
+      //first make the request to the chatbot using unirest
+      
+      // unirest.get('http://api.program-o.com/v2.3.1/chatbot/')
+      //   .field()
+      
+      
+      //crunch message from chatbot using sentiment analasys
+      
+      //get gif based on sentiment analasys results
   });
   
   //what to do when a socket disconnects 
