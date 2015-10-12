192ef10761670dde7b767a4f9ff6dea9f7855d37
Zain Munad <zainm211@hotmail.com>
2015-10-12 04:19:18 +0000

    getting replies from bot works, need to implement gifs

--- server.js
+++ server.js
@@ -27,13 +27,34 @@ io.on('connection', function(socket){
       //send message to chatbot for processing
       //first make the request to the chatbot using unirest
       
-      // unirest.get('http://api.program-o.com/v2.3.1/chatbot/')
-      //   .field()
+      unirest.get('http://demo.program-o.com/b0tco/get_response.php')
+         .query({'say' : msg.message})
+         //.query({'bot_id' : 1})
+         .query({'convo_id' : msg.name})
+         .end(function(res){
+           //if something bad happened break and send an error message back to the client
+           var parsed = JSON.parse(res.body);
+           if(res.error || ~res.body.indexOf('<') || ~parsed.botsay.indexOf('Error') ){
+             console.log("there was a problem b0ss :", res.error);
+             io.emit('chat message', {name: 'system', message: 'there was an error, try sending again'} );
+             
+           } else {
+             console.log("yeee we got a response:", parsed.botsay);
+             
+             crunchSentiment(parsed.botsay, msg.name);
+           }
+         });
       
       
       //crunch message from chatbot using sentiment analasys
+      function crunchSentiment(botreply, username){
+        
+        
+      }
       
       //get gif based on sentiment analasys results
+      
+      //attach gif to message and send it to be free (go fly my creation SPREAD YOUR WINGS)
   });
   
   //what to do when a socket disconnects 
